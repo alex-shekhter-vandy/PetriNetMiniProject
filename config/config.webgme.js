e375942b127a73f17d9c0efc85d5eac5f9346ee4
Alex Shekhter <aleksandr.shekhter@vanderbilt.edu>
2022-08-09 01:28:03 -0400

    Added petrinetvisualizer

--- config/config.webgme.js
+++ config/config.webgme.js
@@ -11,14 +11,17 @@ config.seedProjects.basePaths.push(__dirname + '/../src/seeds/petrinet');
 
 
 
+config.visualization.panelPaths.push(__dirname + '/../src/visualizers/panels');
 
 
 
 
 // Visualizer descriptors
-
+config.visualization.visualizerDescriptors.push(__dirname + '/../src/visualizers/Visualizers.json');
 // Add requirejs paths
 config.requirejsPaths = {
+  'panels': './src/visualizers/panels',
+  'widgets': './src/visualizers/widgets',
   'PeNDeS': './src/common'
 };
 
